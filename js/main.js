a8298433ea6dbeb40d49e2f0b18a0ea0bc679f2b
dao <gary.vo32@gmail.com>
2022-03-15 15:28:06 -0700

    added phone number validation with regex pattern

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// check valid phone number
+/* const telRegex = '(^(1-)?\d{3}-\d{3}-\d{4}$';
+const telInput = document.querySelector('#phoneNum');
+
+telInput.addEventListener('input', checkValidTelly)
+
+function checkValidTelly(e){
+  if(e.target.value == telRegex){
+    console.log('got it')
+  }
+  console.log(e.target.value);
+} */
+
+// check pw match
